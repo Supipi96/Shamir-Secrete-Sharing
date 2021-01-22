b72e9549363534a30b38cdf06cb5e36ce40bf65b
Supipi96 <76469306+Supipi96@users.noreply.github.com>
2021-01-22 22:01:19 +0530

    Server

--- Server/Program.cs
+++ Server/Program.cs
@@ -27,55 +27,94 @@ namespace Server
             Program p = new Program();
 
             Socket[] clientArray = new Socket[10];
+            int count = 0;
+
+            int[,] records = new int[100,100];
 
             while(true)
             {
+
             String command = null;
             Console.WriteLine("split/combine?");
             command = Console.ReadLine();
 
-            // Generate Key 
+            Console.WriteLine("Required SuperNodes to combine: ");
+            string requiredStr = Console.ReadLine();
+            int required = int.Parse(requiredStr);
 
-                int polynomsCount = 3;
-                var byteKey = KeyGenerator.GenerateKey(polynomsCount * 16);
-                var key = KeyGenerator.GenerateDoubleBytesKey(byteKey);
-                Console.WriteLine("Key Generated");
+            // Put key to array
 
             if (String.Equals(command, "split"))
             {
 
-                Console.WriteLine("Number of SuperNodes: ");
+
+                // Recieve key 
+                ClientSocket = ServerListener.Accept();
+                
+                byte[] request = new byte[1024];
+                String message = "send";
+                request = Encoding.Default.GetBytes(message);
+                int size_1 = message.Length;
+                ClientSocket.Send(request, 0,size_1, SocketFlags.None);
+
+                byte[] receivedKey = new byte[1024];
+                int size2 = ClientSocket.Receive(receivedKey);
+                ushort[] key = new ushort[receivedKey.Length / 2];
+                // Buffer.BlockCopy(receivedKey, 0, key, 0, receivedKey.Length);
+                Console.WriteLine("Key received from client");
+
+               /* int polynomsCount = 3;
+                var byteKey = KeyGenerator.GenerateKey(polynomsCount * 16);
+                var key = KeyGenerator.GenerateDoubleBytesKey(byteKey);
+                //byte[] target = new byte[key.Length * 2]; 
+                Console.WriteLine("Key Generated");*/
+
+                Console.WriteLine("Number of SuperNodes to share: ");
                 string playerStr = Console.ReadLine();
-
-                Console.WriteLine("Required SuperNodes for sharing: ");
-                string requiredStr = Console.ReadLine();
-
                 int players = int.Parse(playerStr);
-                int required = int.Parse(requiredStr);
 
                 // Split key
 
                 var splitted = SharesManager.SplitKey(key, players, required);
-            
+
+                Random randNum = new Random();
+                List<int> randomList = new List<int>();
+
+                int number = 0;
+
                 // share splited key parts with clients
 
-                
-
                 for (int j = 0; j < splitted.Length; j++)
-                {
-
-                    int x = j+1;
+                {   
+                         
+                    do {
+                        number = randNum.Next(splitted.Length);
+                        
+                    } while (randomList.Contains(number));
+                    randomList.Add(number);
+                    records[count,j] = number;
+                    
                     ClientSocket = ServerListener.Accept();
-                    clientArray[j] = ClientSocket;
-                    Console.WriteLine(x+"Clients connected");
+                    int clientNum = records[count,j];
+                    clientArray[clientNum] = ClientSocket;
+                    Console.WriteLine(clientNum +"Clients connected");
+
                     byte[] msg = new byte[1024];
                     msg = Encoding.Default.GetBytes(splitted[j]);
                     int size = splitted[j].Length;
-                    clientArray[j].Send(msg, 0, size, SocketFlags.None);
-                    Console.WriteLine("Sent to client ");
-                        
+                    clientArray[clientNum ].Send(msg, 0, size, SocketFlags.None);
+                    Console.WriteLine("Sent key part to client ");
+
+                    String numOfKey =  count.ToString();             
+                    byte[] intBytes = BitConverter.GetBytes(count);
+                    int size_2 = numOfKey.Length;
+                    clientArray[clientNum].Send(intBytes, 0, size_2 , SocketFlags.None);
+                    Console.WriteLine("Sent key number");;
 
                 }
+
+
+                count++;
             }
 
             /*............................COMBINE KEY............................................. */
@@ -83,31 +122,38 @@ namespace Server
             
             if (String.Equals(command, "combine"))
             {
-                Console.WriteLine("Number of shares to generate key: ");
-                string sharesCountStr = Console.ReadLine();
-                int sharesCount = int.Parse(sharesCountStr);
+
+                Console.WriteLine("Number of shares to generate key: "+ required);
+                Console.WriteLine("Number of key: ");
+                string numOfKey = Console.ReadLine();
+                int num = int.Parse(numOfKey);
 
                 string order = null;
                 Console.WriteLine("Enter your order");
                 order = Console.ReadLine();
 
-                var shares = new string[sharesCount];
+                var shares = new string[required];
             
-                for (int i = 0; i < sharesCount; i++)
-                {
-                
+                for (int i = 0; i < required; i++)
+                {                
 
                     byte[] bytes = Encoding.ASCII.GetBytes(order);
                     int size = bytes.Length;
-                    clientArray[i].Send(bytes, 0, size, SocketFlags.None);
+                    int clientNum = records[num,i];
+                    clientArray[clientNum].Send(bytes, 0, size, SocketFlags.None);
                     Console.WriteLine("Sent your order");
 
+                    byte[] intBytes = BitConverter.GetBytes(num);
+                    int size_2 = numOfKey.Length;
+                    clientArray[clientNum].Send(intBytes, 0, size_2 , SocketFlags.None);
+                    Console.WriteLine("Sent key number");
+
                     byte[] msgFromCilent = new byte[1024];
-                    int size2 = clientArray[i].Receive(msgFromCilent);
+                    int size2 = clientArray[clientNum].Receive(msgFromCilent);
                     shares[i] = System.Text.Encoding.ASCII.GetString(msgFromCilent, 0, size2);
                     Console.WriteLine("Msg received from client : " + shares[i]);
 
-                    if (i == sharesCount-1)
+                    if (i == required-1)
                     {
            
                         var generatedKey = SharesManager.CombineKey(shares);
@@ -115,8 +161,7 @@ namespace Server
                         Console.WriteLine("\nGenerated key:",generatedKey);
                         Console.WriteLine();
 
-                    }
-                    
+                    }  
                    
                 }
             }
